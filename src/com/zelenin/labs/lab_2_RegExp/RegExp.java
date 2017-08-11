861d9df24db274f39ca60a48c1314aafcb45280c
baronboroda <mj3@ukr.net>
2017-08-11 21:54:32 +0300

    Commit 4

--- src/com/zelenin/labs/lab_2_RegExp/RegExp.java
+++ src/com/zelenin/labs/lab_2_RegExp/RegExp.java
@@ -7,9 +7,9 @@ import java.util.regex.Pattern;
 public class RegExp {
     public static void main(String[] args) {
 
-        String input = " ...Hello, my name is Max! I attend \"Big Data\" courses in Epam IT Academy. " +
-                        "This is my text example for the string processing task. Hello (just repeat first word). ";
-       /*
+        String input = "Hello, my name is Max! I attend \"Big Data\" courses in Epam IT Academy. " +
+                        "This is my text example for the string processing task. Hello (just repeat first word).";
+
         System.out.println("------------Input text--------------");
         System.out.println(input);
         System.out.println();
@@ -19,13 +19,12 @@ public class RegExp {
 
         System.out.println();
         printFirstUniqueWord(input);
-*/
-        Pattern pattern = Pattern.compile("[^.:!?\\s][^.!?]*[.!?]?(?=\\s|$)");
-        Matcher matcher = pattern.matcher(input);
 
-        while(matcher.find()) {
-            System.out.println(matcher.group());
-        }
+        //System.out.println(splitTextIntoSentences(input));
+
+        System.out.println();
+        System.out.println(changeWordsInSentences(input));
+
     }
 
     public static void printWordsFrequency(String text) {
@@ -51,7 +50,7 @@ public class RegExp {
         return result;
     }
 
-    public static String getFirstUniqueWord(String[] words, HashSet<String> distinct) {
+    public static String getFirstUniqueWord(String[] words, String[] distinct) {
         String result = "";
         for (String d : distinct) {
             int quantity = 0;
@@ -70,24 +69,67 @@ public class RegExp {
         return result;
     }
 
-    public static LinkedHashSet<String> getDistinctWords(String text) {
+    public static String[] getDistinctWords(String text) {
         String[] words = splitTextIntoWords(text);
         List<String> myList = Arrays.asList(words);
         LinkedHashSet<String> mySet = new LinkedHashSet<>(myList);
 
-        return mySet;
+        return mySet.toArray(new String[mySet.size()]);
     }
 
     public static String[] splitTextIntoWords(String text) {
-        text = text.replaceAll("[,.:!?]", " ");
+        text = text.replaceAll("[,.:!?/(/)]", " ");
         String[] words = text.split("\\s+");
         return words;
     }
 
     public static Collection<String> splitTextIntoSentences(String text) {
 
-        Collection<String> sentences = null;
+        Collection<String> sentences = new ArrayList<>();
 
+        Pattern pattern = Pattern.compile("[^.:!?\\s][^.!?]*[.!?]?(?=\\s|$)");
+        Matcher matcher = pattern.matcher(text);
+
+        while(matcher.find()) {
+            sentences.add(matcher.group());
+        }
         return sentences;
+    }
+
+    public static String findFirstShortestWord(String sentence) {
+        String result = "";
+
+        String[] words = getDistinctWords(sentence);
+        result = words[0];
+        for(String w : words) {
+            if(result.length() < w.length()) {
+                result = w;
+            }
+        }
+        return result;
+    }
+
+    public static String findFirstLongestWord(String sentence) {
+        String result = "";
+
+        String[] words = getDistinctWords(sentence);
+        result = words[0];
+        for(String w : words) {
+            if(result.length() > w.length()) {
+                result = w;
+            }
+        }
+        return result;
+    }
+
+    public static String changeWordsInSentences(String text) {
+        String result = "";
+
+        for(String sentence : splitTextIntoSentences(text)) {
+            result += sentence.replaceAll(findFirstLongestWord(sentence), findFirstShortestWord(sentence));
+        }
+
+        return result;
+
     }
 }
