be896cfcc9de1f245d036d6f3fa2e590414429d8
baronboroda <mj3@ukr.net>
2017-08-11 17:53:07 +0300

    RegExp commit 3

--- src/com/zelenin/labs/lab_2_RegExp/RegExp.java
+++ src/com/zelenin/labs/lab_2_RegExp/RegExp.java
@@ -1,34 +1,33 @@
 package com.zelenin.labs.lab_2_RegExp;
 
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.Collections;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Arrays;
 
 public class RegExp {
     public static void main(String[] args) {
 
         String input = "Hello, my name is Max I'm 26 years old. name, Hello my.";
+        printWordsFrequency(input);
 
-        /*Matcher matcher = Pattern.compile("(?\b\w+\b)(?!.+\b\k\b)").matcher(input);
-        while (matcher.find()) {
-            System.out.println(matcher.group());
-        }*/
 
-        input = input.replaceAll("[!?.,]", "");
-        String[] words = input.split("\\s+");
-        
 
     }
 
-    /*public static String[] getUniqueWords(String[] arg) {
+    public static void printWordsFrequency(String text) {
 
-    }*/
+        text = text.replaceAll("[!?.:,]", "");
+        String[] words = text.split("\\s+");
 
-    public static int getFrequency(String[] words, String word) {
+        List<String> myList = Arrays.asList(words);
+        Set<String> mySet = new HashSet<String>(myList);
+
+        for(String s : mySet) {
+            System.out.println(s + " --> " + getWordFrequency(words, s));
+        }
+    }
+
+    public static int getWordFrequency(String[] words, String word) {
         int result = 0;
         for (String s : words) {
             if (s.equals(word)) {
@@ -38,15 +37,4 @@ public class RegExp {
         return result;
     }
 
-    public static boolean findWord(String[] arg, String word) {
-        boolean result = false;
-        for (String s : arg) {
-            if (s.equals(word)) {
-                result = true;
-            } else {
-                result = false;
-            }
-        }
-        return result;
-    }
 }
